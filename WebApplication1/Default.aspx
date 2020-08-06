863a31747181b9d8e034652d54e8de1225f95aa5
GaryGiron <gsteph393@gmail.com>
2020-08-06 09:22:10 -0600

    Initial commit

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Página principal" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1>CLIENTE SA <span> cree sus contactos y visualice la data</span> </h1>
+            </hgroup>
+            <p>
+                Primera tarea del laboratorio de Software avanzado</p>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <ol class="round">
+        <li class="one">
+            <h5>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="Label1" runat="server" Text="Nombre: "></asp:Label>
+                &nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                &nbsp;&nbsp;
+                <asp:Button ID="Button2" runat="server" Text="Crear Contacto" OnClick="Button2_Click" />
+            </h5>
+        </li>
+        <li class="two">
+            <h5>
+                <asp:Button ID="Button1" runat="server" Text="Visualizar contactos" OnClick="Button1_Click" />
+            </h5>
+        </li>
+        <li class="three">
+            <asp:Table ID="myTable" runat="server" Width="100%"> 
+                <asp:TableRow> 
+                 <asp:TableCell>Contactos</asp:TableCell>  
+                </asp:TableRow> 
+            </asp:Table>
+        </li>
+    </ol>
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        //La instancia al servicio se crea global para tener un acceso en los dos metodos que se utilizan
+        //o si es el caso, en todos los que se lleguen a agregar
+        ServiceReference1.administratorcontact100Client admin = new ServiceReference1.administratorcontact100Client();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //El servicio se integro al proyecto como referencia externa
+            /*
+             Si se quiere revisar el mismo se encuentra como ServiceReference1, en la seccion de Service References
+             */
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //cadena de mensaje para indicar si fue creado o no el cliente
+            String mensajecreate = "No fue posible crear el cliente";
+            //validacion de creacion de cliente y envio de data al web service
+            if (admin.create("201403997" + TextBox1.Text, 0, null, 0))
+            {
+                //creacion exitosa
+                mensajecreate = "El contacto se creo Exitosamente";
+            }
+            //Creacion de Alert que se mostrara a usuario al crear o no el contacto
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + mensajecreate + "');", true);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //consumo de Web Service para recibir la data del web service
+            ServiceReference1.readListResponse_list_item[] datos =admin.readList(0,1000000,"201403997",null,null,null,null);
+            //recorrido del arreglo para la creacion dinamica de tabla con los contactos
+            for (int i = 0; i < datos.Length; i++)
+            {
+                TableRow row = new TableRow();
+                TableCell cell1 = new TableCell();
+                cell1.Text = datos[i].name;
+                row.Cells.Add(cell1);
+                //Adicion de filas
+                myTable.Rows.Add(row); 
+            }
+        }
+    }
+}
